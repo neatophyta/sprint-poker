7227641f828a6e1b3bf767bc2582c7a06ac37cb9
Charli Watson <charliroot@gmail.com>
2019-02-25 16:10:43 -0700

    Sprint poker app initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="header">
+      Planning Poker
+    </div>
+    <div class="content">
+      <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+      <CardList/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import CardList from './components/CardList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    CardList
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  html, body {
+     height: 100%;
+     width: 100%;
+     margin: 0;
+     padding: 0;
+  }
+
+  #app {
+    background-color: #f7f7f7;
+    color: #616A6B;
+    font-family: 'Roboto', sans-serif;
+    height: 100%; 
+    margin: 0;
+    text-align: center;
+  }
+
+  .header {
+    background-color: #eeeeee;
+    font-size: 2.5em;
+    height: 125px;
+    padding: 30px;
+      position: relative;
+  }
+
+  .link-button {
+    text-decoration: none;
+      color: #b4b4b4;
+      padding: 10px;
+      border: 1px solid #b4b4b4;
+      background-color: #fff;
+      text-transform: uppercase;
+      border-radius: 3px;
+      display: inline-block;
+  }
+
+  .content {
+    align-items: center;
+    background-color: #ffffff;
+    border-radius: 3px;
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+    position: relative;
+    top: -5em;
+      width: 90%;  
+  }
+
+  .hide {
+    display: none;
+  }
 </style>
